587dc8e8f0c5b6c395a90cfe371b171701d0bf70
JanMeyfarth1999 <jan_meyfarth@teilnehmer.cdemy.de>
2026-09-07 14:53:12 +0200

    mit kalender beginnen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting('de_DE', null);
+
   runApp(const MyApp());
 }
 
@@ -38,10 +43,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  DateTime displayedMonth = DateTime.now();
  
 
   @override
   Widget build(BuildContext context) {
+     String monthTitle =
+      DateFormat('MMMM yyyy', 'de_DE').format(displayedMonth);
 
     return Scaffold(
       appBar: AppBar(
@@ -62,9 +70,10 @@ class _MyHomePageState extends State<MyHomePage> {
                ),
         
         ),
+        
       
     
-        Text(      
+      Text(      
         widget.title,
         style: TextStyle(
           fontSize: 23,
@@ -79,24 +88,128 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     ),
       
-        flexibleSpace: Image.asset('assets/gta6_theme.jpg',
+        flexibleSpace: Image.asset('assets/gta6_theme.png',
         width: double.infinity,
         fit: BoxFit.cover,
         
         ),
         
       ),
-      body: Center(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+   
+    
+
+
      
         child: Column(
+          
 
           mainAxisAlignment: .center,
           children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Mo',
+                style: TextStyle(
+                  color: Colors.white,
+                   fontWeight: FontWeight.bold,
+                ),
+                ),
+                Text('Di',
+                 style: TextStyle(
+                  color: Colors.white,
+                   fontWeight: FontWeight.bold,
+                ),
+                ),
+                Text('Mi',
+                 style: TextStyle(
+                  color: Colors.white,
+                   fontWeight: FontWeight.bold,                  
+                ),
+                ),
+                Text('Do',
+                 style: TextStyle(
+                  color: Colors.white,
+                   fontWeight: FontWeight.bold,
+                ),
+                ),
+                Text('Fr',
+                 style: TextStyle(
+                  color: Colors.white,
+                   fontWeight: FontWeight.bold,
+                ),
+                ),
+                Text('Sa',
+                 style: TextStyle(
+                  color: Colors.white,
+                   fontWeight: FontWeight.bold,
+                ),
+                ),
+                Text('So',
+                 style: TextStyle(
+                  color: Colors.white,
+                   fontWeight: FontWeight.bold,
+                ),
+                ),
+              
+              ],
+
+            ),
+
+            SizedBox(
+              height: 700,
+            ),
+          
+           Stack(
+          children: [
+            Text(
+              monthTitle,
+                style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Righteous',
+                 foreground: Paint()
+            ..style = PaintingStyle.stroke
+            ..color = Colors.black
+            ..strokeWidth = 4,
+
+                
+                   
+                ),
+            ),
+          
+            
+
+
+            Text(
+              monthTitle,
+                style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Righteous',
+                color: Colors.white,
+                   
+                ),
+
+
+             ),
          
           ],
         ),
+          ],
+      ),
       ),
    
     );
+    
+    
   }
 }
