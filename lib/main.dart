0b7a1efb2394adecbc3ece94cdfcb6c37eab85b0
JanMeyfarth1999 <jan_meyfarth@teilnehmer.cdemy.de>
2026-09-07 11:38:13 +0200

    AppBar Background

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     DateTime now = DateTime.now();
     String formattedDate = DateFormat('dd.MM.yyyy').format(now);
-    
+
     return MaterialApp(
 
       title: 'Flutter Demo',
@@ -45,10 +45,21 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-      
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
-        title: Text(widget.title),
+        centerTitle: true,
+        title: Text(
+        widget.title,
+        style: TextStyle(
+          fontSize: 23,
+          fontWeight: FontWeight.bold,
+
+            ),
+        ),
+        flexibleSpace: Image.asset('assets/gta6_theme.jpg',
+        width: double.infinity,
+        fit: BoxFit.cover,
+        
+        ),
+        
       ),
       body: Center(
      
